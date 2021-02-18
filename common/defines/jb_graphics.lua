77dcb1307eaecdf8792e5a66cc312200d8aa4e16
Jingo Spunk <30711461+JingoSpunk@users.noreply.github.com>
2021-02-18 06:21:15 -0700

    gfx mod

---
+++ common/defines/jb_graphics.lua
@@ -0,0 +1,6 @@
+--	NDefines_Graphics.NGraphics.
+	
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0; -- 400
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0; -- 350
+--NDefines.NGraphics.GRADIENT_BORDERS_CAMERA_DISTANCE_OVERRIDE_COUNTRY = 0.01
+NDefines.NGraphics.DRAW_DETAILED_CUTOFF = 100;
---
+++ hgographics.mod
@@ -0,0 +1,7 @@
+name="MappaMundi Graphics Submod"
+path="mod/MappaMundiGFX/"
+tags={
+	"Graphics"
+	"Map"
+}
+supported_version="1.10.*"
