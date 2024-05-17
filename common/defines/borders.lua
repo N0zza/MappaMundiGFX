3ef3689486148265788215102ba17b4013fa7a3f
Jingo Spunk <30711461+JingoSpunk@users.noreply.github.com>
2024-05-16 18:34:15 -0700

    Experimental changes for Faaz

--- common/defines/borders.lua
+++ common/defines/borders.lua
@@ -5,3 +5,16 @@ NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_MAX_VICTORY_POINTS_FOR_PERCEN
 NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 1000
 NDefines_Graphics.NGraphics.STATE_BORDER_FADE_NEAR = 250
 NDefines_Graphics.NGraphics.STATE_BORDER_FADE_FAR = 400
+
+--experimental
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_COUNTRY_CENTER_THICKNESS = 0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_DIPLOMACY = 0
+NDefines_Graphics.NGraphics.BORDER_WIDTH = 0.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_COUNTRY = 0.98
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_OUTLINE_CUTOFF_DIPLOMACY = 0.98
+NDefines_Graphics.NMapMode.SELECTED_COUNTRY_HIGHLIGHT_THICKNESS_MULT = 1.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_LOW = 0.0 -- country area in sum of pixels ...
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_HIGH = 0.0 -- ... the value is squared, so fe. country of size 100x100pix = 10000
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_LOW = 0.0 -- thickness in pixels
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_COUNTRY_HIGH = 0.0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_THICKNESS_STATE = 0.0
